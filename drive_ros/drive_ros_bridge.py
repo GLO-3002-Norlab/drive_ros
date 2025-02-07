eefde2aafe976c5a28a6380f2db92a1f647c5ad1
Your Name <you@example.com>
2025-02-07 16:33:47 -0500

    Empty python package

---
+++ drive_ros/drive_ros_bridge.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from drive_ros.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'drive_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='norlab',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drive_ros_bridge = drive_ros.drive_ros_bridge:main'
+        ],
+    },
+)
